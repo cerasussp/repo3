6a5859adef75f005e860c24639196622a0fde2a8
asuka <1553534610@qq.com>
2019-06-24 20:30:39 +0800

    上传

---
+++ src/com/itcast/test/Test.java
@@ -0,0 +1,12 @@
+package com.itcast.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hahha");
+        System.out.println("hahha");
+        System.out.println("hahha");
+        System.out.println("hahha");
+        System.out.println("hahha");
+        System.out.println("hahha");
+    }
+}
